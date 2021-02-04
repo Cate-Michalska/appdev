907b3d23347e09eb0fa8dd2f178c4e7bcb45290a
Catherine Lukner <catluk@localhost.localdomain>
2021-02-04 07:04:59 -0600

    Worked on database migrations up until chat conversation members

---
+++ Phoenix/curious_messenger/lib/curious_messenger/chat/conversation_member.ex
@@ -0,0 +1,28 @@
+defmodule CuriousMessenger.Chat.ConversationMember do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias CuriousMessenger.Auth.User
+  alias CuriousMessenger.Chat.Conversation
+
+  schema "chat_conversation_members" do
+    field :owner, :boolean, default: false
+    # field :conversation_id, :id
+    # field :user_id, :id
+
+    belongs_to :user, User
+    belongs_to :conversation, Conversation
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(conversation_member, attrs) do
+    conversation_member
+    |> cast(attrs, [:owner, :conversation_id, :user_id])
+    |> validate_required([:owner, :conversation_id, :user_id])
+    |> unique_constraint(:user, name: :chat_conversation_members_conversation_id_user_id_index)
+    |> unique_constraint(: conversation_id,
+      name: :chat_conversation_members_owner
+    )
+  end
+end
---
+++ Phoenix/curious_messenger/priv/repo/migrations/20210204123957_create_chat_emojis.exs
@@ -0,0 +1,13 @@
+defmodule CuriousMessenger.Repo.Migrations.CreateChatEmojis do
+  use Ecto.Migration
+
+  def change do
+    create table(:chat_emojis) do
+      add :key, :string
+      add :unicode, :string
+
+      timestamps()
+    end
+
+  end
+end
