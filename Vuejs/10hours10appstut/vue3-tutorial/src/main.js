6a8a5a81c68f680500e5261b3788e8d6b2a095e4
Catherine Lukner <catluk@localhost.localdomain>
2021-04-21 15:26:37 -0500

    Deleted failed projects and started 10 apps in 10 hours vue tutorial

---
+++ Vuejs/10hours10appstut/vue3-tutorial/src/main.js
@@ -0,0 +1,14 @@
+const template = `<div>
+<h1>{{ name }}</h1>
+</div>`; 
+
+const data = function data(){
+    return {
+        title:"Vue3 Tutorial", 
+        name: "Cate"
+    }; 
+};  
+
+const App = { data, template }; 
+
+Vue.createApp(App).mount('#vue-app'); 
---
+++ Vuejs/10hours10appstut/vue3/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 v-show="isShowing">{{ count }}</h1>
+  <!-- <h3 v-else-if="isShowing === null">This is isShowing as null</h3>
+  <h2 v-else>This is v-else part</h2> -->
+  <p>Something Else</p>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      title: "<h1>Hey Vue3</h1>", 
+      isShowing: false, 
+      count: 0, 
+    }; 
+  }, 
+  mounted(){
+    setInterval(() => {
+      this.count++; 
+    }, 1000); 
+    } 
+};   
+</script>
