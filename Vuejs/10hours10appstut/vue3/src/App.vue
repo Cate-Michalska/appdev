c1ae390282f3a8ef3a7bc5b7a71b66e12a2a4986
Catherine Lukner <catluk@localhost.localdomain>
2021-04-24 18:27:55 -0500

    Completed DcHeros App

--- Vuejs/10hours10appstut/vue3/src/App.vue
+++ Vuejs/10hours10appstut/vue3/src/App.vue
@@ -1,23 +1,15 @@
 <template>
-  <h1 v-show="isShowing">{{ count }}</h1>
-  <!-- <h3 v-else-if="isShowing === null">This is isShowing as null</h3>
-  <h2 v-else>This is v-else part</h2> -->
-  <p>Something Else</p>
+  <AppHeader />
+  <div class="w-full flex">
+    <DcHeros />
+  </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue'; 
+import DcHeros from './components/DcHeros.vue'; 
+
 export default {
-  data(){
-    return{
-      title: "<h1>Hey Vue3</h1>", 
-      isShowing: false, 
-      count: 0, 
-    }; 
-  }, 
-  mounted(){
-    setInterval(() => {
-      this.count++; 
-    }, 1000); 
-    } 
-};   
+  components: { AppHeader, DcHeros }, 
+}; 
 </script>
---
+++ Vuejs/10hours10appstut/vue3/src/components/DcHeros.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="m-auto">
+      <h1 class="text-2xl text-center">{{ herosCount }} Dc Heros</h1>
+      <ul>
+        <li class="flex justify-between" v-for="(hero, index) in dcHeros" v-bind:key="hero.name">
+          <div>{{ hero.name }}</div>
+          <button v-on:click="remove(index)">x</button>
+        </li>
+      </ul>
+      <form class="mt-10" @submit.prevent="addHero">
+        <input 
+          class="border rounded" 
+          v-model="newHero" 
+          placeholder="Type Hero Name Here"
+        />
+        <button 
+        class="border rounded bg-gradient-to-r from-red-700 to-pink-500 text-white"
+        type="submit">
+          Add Hero
+        </button>
+      </form>
+    </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    herosCount() {
+      return this.dcHeros.length; 
+    }
+  },
+  methods: {
+     addHero() {
+       if(this.newHero != '') {
+        this.dcHeros.push({ name: this.newHero });
+        this.newHero = ""; 
+       } 
+     }, 
+     remove(index) {
+       this.dcHeros = this.dcHeros.filter((hero, i) => i != index)
+     }, 
+  }, 
+  data() {
+    return {
+      newHero: "", 
+      dcHeros: [
+        { name: "SuperGirl" }, 
+        { name: "Flash" },  
+        { name: "Batman" },
+        { name: "Arrow" },
+        { name: "SuperMan" },
+        ], 
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
