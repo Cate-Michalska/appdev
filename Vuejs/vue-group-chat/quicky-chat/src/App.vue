4dd6278ef34e39ea9af8a951bce36e9bd14c8091
Catherine Lukner <catluk@localhost.localdomain>
2021-04-25 23:10:54 -0500

    Attempted to add Quick Chat Component, once again, and once again, failed

---
+++ Vuejs/vue-group-chat/quicky-chat/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <QuickChat />
+</template>
+
+<script>
+import QuickChat from './components/QuickChat.vue'
+
+export default {
+  name: 'App',
+  components: {
+    QuickChat
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vuejs/vue-group-chat/quicky-chat/src/components/QuickChat.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <Chat 
+       :participants="participants"
+       :myself="myself"
+       :messages="messages"
+       :onType="onType"
+       :onMessageSubmit="onMessageSubmit"
+       :chatTitle="chatTitle"
+       :placeholder="placeholder"
+       :colors="colors"
+       :borderStyle="borderStyle"
+       :hideCloseButton="hideCloseButton"
+       :closeButtonIconSize="closeButtonIconSize"
+       :submitIconSize="submitIconSize"/>
+   </div>
+</template>
+
+<script>
+import { Chat } from 'vue-quick-chat'
+export default {
+  components: {
+    Chat
+  },
+}
+</script>
+
+<style>
+
+</style>
